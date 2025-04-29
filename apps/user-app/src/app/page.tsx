5dc88519995f34c0bac1044fe370e65ac09eae55
Vishal Patel <85049843+venteury@users.noreply.github.com>
2025-04-29 12:51:01 +0530

    feat: prisma dependency config

--- apps/user-app/src/app/page.tsx
+++ apps/user-app/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl border-2 p-3 rounded-sm shadow-md">Hello Vishal</h1>
+      <h1 className="text-4xl border-2 p-3 rounded-sm shadow-md">
+        Hello Vishal
+      </h1>
     </div>
   );
 }
