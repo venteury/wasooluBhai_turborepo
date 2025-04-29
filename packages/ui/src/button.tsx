f9bd3ecf3ec20494c3287ee40e4cda3ee574f42d
Vishal Patel <85049843+venteury@users.noreply.github.com>
2025-04-29 11:57:38 +0530

    feat: prisma init

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,19 +1,15 @@
 "use client";
-
 import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
+  onClick?: () => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+    <button className={className} onClick={onClick}>
       {children}
     </button>
   );
